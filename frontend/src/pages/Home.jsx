47bb7c8812239d1afcba8cd706d9ec4389cff996
Rasal Hossain Adib <adibrasel.2022@gmail.com>
2023-08-26 02:09:59 +0600

    frontend
    frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,84 @@
 import React from 'react'
 
+import heroImage01 from "../assets/images/hero-img01.png"
+import heroImage02 from "../assets/images/hero-img02.png"
+import heroImage03 from "../assets/images/hero-img03.png"
+
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return (<>
+
+  {/* ============== Hero Section ============= */}
+  <>
+  <section className='hero__section pt-[60px] 2xl:h-[800px]'>
+    <div className="container">
+      <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+
+      {/* ========== hero content start =========== */}
+      <div>
+        <div className='lg:w-[570px] '>
+          <h1 className='text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]'>We help patients live a healthy, longer life. </h1>
+          <p className='text__para'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, quibusdam voluptates? Ipsam nisi non explicabo cumque blanditiis, beatae ducimus, accusamus ad esse quod quas laborum autem in consectetur tenetur dignissimos aliquid placeat. Et dolores, obcaecati optio voluptatibus nemo architecto corrupti ea doloribus. Impedit a aut, dicta voluptatibus cupiditate fuga dolor?
+          </p>
+
+          <button className='btn'>Request an Appointment</button>
+        </div>
+
+        {/* =========== hero counter =========== */}
+        <div className='mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px] '>
+          <div>
+            <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700]  text-headingColor'>
+              30+
+            </h2>
+            <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px] '></span>
+            <p className='text__para'>Years of Experience</p>
+          </div>
+
+          <div>
+            <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700]  text-headingColor'>
+              15+
+            </h2>
+            <span className='w-[100px] h-2 bg-purpleColr rounded-full block mt-[-14px] '></span>
+            <p className='text__para'>Clinic Location</p>
+          </div>
+
+          <div>
+            <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700]  text-headingColor'>
+              100+
+            </h2>
+            <span className='w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px] '></span>
+            <p className='text__para'>Patient Satisfaction</p>
+          </div>
+
+        </div>
+
+      </div>
+      {/* ========== hero content end =========== */}
+
+
+
+      <div className='flex gap-[30px] justify-end'>
+        <div>
+          <img className='w-full' src={heroImage01} alt="" />
+        </div>
+        <div className='mt-[30px]'>
+          <img src={heroImage02} alt="" className='w-full mb-[30px] ' />
+          <img src={heroImage03} alt="" className='w-full ' />
+        </div>
+      </div>
+
+
+      </div>
+    </div>
+  </section>
+
+
+  </>
+  {/* ============== Hero Section end ============= */}
+
+
+  
+  </>)
 }
 
 export default Home
