3fe24fe6ee5dfd6e8cd14595774bdba8c071802e
Rasal Hossain Adib <adibrasel.2022@gmail.com>
2023-08-25 23:38:29 +0600

    frontend
    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import "./app.css";
+// import Layout from "./layout/Layout.jsx";
+
+import Layout from "./layout/Layout.jsx"
+
+function App() {
+
+  return (
+    <>
+      <Layout />
+    </>
+  )
+  
+}
+
+export default App
