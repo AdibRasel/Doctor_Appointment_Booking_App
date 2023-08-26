04a5075fb5ca4a0cbbe469ea32a9c3e7a09050e6
Rasal Hossain Adib <adibrasel.2022@gmail.com>
2023-08-26 23:04:32 +0600

    frontend
    frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,13 @@ import heroImage01 from "../assets/images/hero-img01.png"
 import heroImage02 from "../assets/images/hero-img02.png"
 import heroImage03 from "../assets/images/hero-img03.png"
 
+import icon01 from "../assets/images/icon01.png"
+import icon02 from "../assets/images/icon02.png"
+import icon03 from "../assets/images/icon03.png"
+
+import {Link} from "react-router-dom"
+import {BsArrowRight} from "react-icons/bs"
+
 const Home = () => {
   return (<>
 
@@ -76,6 +83,78 @@ const Home = () => {
   </>
   {/* ============== Hero Section end ============= */}
 
+
+    <section>
+      <div className="container">
+        <div className='lg:w-[470px] mx-auto'>
+          <h2 className='heading text-center'>Providing the best medical services</h2>
+          <p className='text__para text-center'>World-class care for everyone. Our health System offers unmatched, expert health care. </p>
+        </div>
+
+        {/* <div className="flex flex-wrap items-center justify-between flex-col md:flex-row gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+
+        </div> */}
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+        
+        {/* =========item 1 start============= */}
+          <div className='py=[30px] px-5 '>
+            <div className='flex items-center justify-center '>
+              <img src={icon01} alt="" />
+            </div>
+
+            <div className='mt-[30px]'>
+              <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Find a Doctor</h2>
+              <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World-class care for everyone. Our health System offers unmatched, expert health care. From the lav to the clinic.</p>
+
+              <Link to="/doctors" className='w-[44px] h-[44px] rounded-full border border-solid border-[#141A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none '>
+                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+              </Link>
+            </div>
+          </div>
+        {/* =========item 1 end============= */}
+
+          {/* =========item 2 start============= */}
+          <div className='py=[30px] px-5 '>
+            <div className='flex items-center justify-center '>
+              <img src={icon02} alt="" />
+            </div>
+
+            <div className='mt-[30px]'>
+              <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Find a Location</h2>
+              <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World-class care for everyone. Our health System offers unmatched, expert health care. From the lav to the clinic.</p>
+
+              <Link to="/doctors" className='w-[44px] h-[44px] rounded-full border border-solid border-[#141A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none '>
+                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+              </Link>
+            </div>
+          </div>
+        {/* =========item 2 end============= */}
+
+
+          {/* =========item 3 start============= */}
+          <div className='py=[30px] px-5 '>
+            <div className='flex items-center justify-center '>
+              <img src={icon03} alt="" />
+            </div>
+
+            <div className='mt-[30px]'>
+              <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Book Appointment</h2>
+              <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>World-class care for everyone. Our health System offers unmatched, expert health care. From the lav to the clinic.</p>
+
+              <Link to="/doctors" className='w-[44px] h-[44px] rounded-full border border-solid border-[#141A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none '>
+                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+              </Link>
+            </div>
+          </div>
+        {/* =========item 3 end============= */}
+
+
+        </div>
+
+
+      </div>
+    </section>
 
   
   </>)
